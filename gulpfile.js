09265b32742e9a1451819b1f85ff811cbcac5639
lucas wanderosck <lucaskleinwanderosck@gmail.com>
2023-06-06 15:40:05 -0300

    setup do projeto

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+
+function styles() {
+  return gulp
+    .src("./src/styles/*.scss")
+    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(gulp.dest("./dist/css"));
+}
+exports.default = function () {
+  gulp.watch("./src/styles/*.scss", gulp.parallel(styles));
+};
