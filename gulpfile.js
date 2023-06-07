8289526190295e2c9b78920358fb7f7088c8c264
lucas wanderosck <lucaskleinwanderosck@gmail.com>
2023-06-07 13:54:43 -0300

    criando o hero

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,26 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const imagemin = require("gulp-imagemin");
 
+// Função para o sass
 function styles() {
   return gulp
     .src("./src/styles/*.scss")
     .pipe(sass({ outputStyle: "compressed" }))
     .pipe(gulp.dest("./dist/css"));
 }
-exports.default = function () {
+
+// Função para comprimir as imagens
+function comprimeImg() {
+  return gulp
+    .src("./src/images/**/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("./dist/images"));
+}
+
+// exportação
+exports.default = gulp.parallel(styles, comprimeImg);
+
+exports.watch = function () {
   gulp.watch("./src/styles/*.scss", gulp.parallel(styles));
 };
