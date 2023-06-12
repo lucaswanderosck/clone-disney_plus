4877b31a57944991299933161181ac85eedac4ec
lucas wanderosck <lucaskleinwanderosck@gmail.com>
2023-06-12 12:08:05 -0300

    configurando o uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const imagemin = require("gulp-imagemin");
+const uglify = require("gulp-uglify");
 
 // Função para o sass
 function styles() {
@@ -18,9 +19,18 @@ function comprimeImg() {
     .pipe(gulp.dest("./dist/images"));
 }
 
+// Função para minificar o JavaScript
+function comprimeJs() {
+  return gulp
+    .src("./src/scripts/*.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./dist/js"));
+}
+
 // exportação
-exports.default = gulp.parallel(styles, comprimeImg);
+exports.default = gulp.parallel(styles, comprimeImg, comprimeJs);
 
 exports.watch = function () {
   gulp.watch("./src/styles/*.scss", gulp.parallel(styles));
+  gulp.watch("./src/scripts/*.js", gulp.parallel(comprimeJs));
 };
